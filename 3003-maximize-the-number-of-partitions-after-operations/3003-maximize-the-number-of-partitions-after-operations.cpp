381068874e9586dbec9bd083facf96992825383f
Aryan Pundir <95073218+Pundiraryan@users.noreply.github.com>
2024-01-11 13:43:59 +0530

    Time: 603 ms (35.12%), Space: 160.9 MB (11.96%) - LeetHub

---
+++ 3003-maximize-the-number-of-partitions-after-operations/3003-maximize-the-number-of-partitions-after-operations.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    unordered_map<long long,int>dp;
+    int f(long long i,long long uq,int c,int k,string &s){
+        if(i>=s.length())return 0;
+         long long key=(i<<27)|(uq<<1)|c;
+        if(dp.find(key)!=dp.end())return dp[key];
+        int cidx=s[i]-'a';
+        int nuq=uq|(1<<cidx);
+        int uc=__builtin_popcount(nuq);
+        int result=0;
+        if(uc>k){
+            result=1+f(i+1,1<<cidx,c,k,s);
+        }else{
+            result=f(i+1,nuq,c,k,s);
+        }
+        if(c){
+            for(int ch=0;ch<26;ch++){
+                int nnuq=uq|(1<<ch);
+                int nuc=__builtin_popcount(nnuq);
+                if(nuc>k){
+                    result=max(result,1+f(i+1,1<<ch,0,k,s));
+                }else{
+                    result=max(result,f(i+1,nnuq,0,k,s));
+                }
+            }
+        }
+        return dp[key]=result;
+    }
+    int maxPartitionsAfterOperations(string s, int k) {
+        return 1+f(0,0,1,k,s);
+    }
+};
