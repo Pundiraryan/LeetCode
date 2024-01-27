d793de62e344d782ffd0d2eb8f96222e1febdaa7
Aryan Pundir <95073218+Pundiraryan@users.noreply.github.com>
2024-01-27 12:39:28 +0530

    Time: 29 ms (52.20%), Space: 11.7 MB (39.56%) - LeetHub

--- 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
@@ -7,10 +7,9 @@ public:
         if(n<=0)return 0;
         if(k<0)return 0;
         if(dp[n][k]!=-1)return dp[n][k];
-        int ans=0;
-        for(int i=0;i<=min(n-1,k);i++){
-            ans=(ans+f(n-1,k-i))%mod;
-        }
+        int ans=f(n-1,k);
+        ans=(ans+f(n,k-1))%mod;//sliding window
+        if(k>=n)ans=(ans-f(n-1,k-n)+mod)%mod;
         return dp[n][k]=ans;
   }
     int kInversePairs(int n, int k) {
