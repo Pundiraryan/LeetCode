600df050e08f5f9c157d28a9487bd9e41649afe6
Aryan Pundir <95073218+Pundiraryan@users.noreply.github.com>
2024-01-31 11:41:14 +0530

    Time: 6 ms (79.86%), Space: 58.5 MB (81.94%) - LeetHub

---
+++ 2856-minimum-array-length-after-pair-removals/2856-minimum-array-length-after-pair-removals.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minLengthAfterRemovals(List<Integer> nums) {
+        int n=nums.size();
+        int i=0,j=(n+1)/2;
+        int ans=n;
+        while(i<n/2 && j<n){
+            if(nums.get(i)<nums.get(j)){
+                ans-=2;
+            }
+            i++;j++;
+        }
+        return ans;
+    }
+}
