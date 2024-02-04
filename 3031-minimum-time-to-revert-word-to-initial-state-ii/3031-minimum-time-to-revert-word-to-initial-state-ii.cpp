d39788d5d4fac6c3ee4f5d168f3d9a51a0438483
Aryan Pundir <95073218+Pundiraryan@users.noreply.github.com>
2024-02-04 17:17:30 +0530

    Time: 102 ms (65.38%), Space: 43.5 MB (19.23%) - LeetHub

--- 3031-minimum-time-to-revert-word-to-initial-state-ii/3031-minimum-time-to-revert-word-to-initial-state-ii.cpp
+++ 3031-minimum-time-to-revert-word-to-initial-state-ii/3031-minimum-time-to-revert-word-to-initial-state-ii.cpp
@@ -1,38 +1,31 @@
-typedef long long int ll;
-
-const int M = 1e9+9;
-const ll P = 31;
-
 class Solution {
-    
-    vector<ll> prefix_hash;
-    vector<ll> p_pow;
-    void FillHash(const string & s) {
-        int n = s.size();
-        
-        p_pow.resize(n+1), prefix_hash.resize(n+1);
-
-        p_pow[0] = 1; 
-        for (int i = 1; i <= n; i++) 
-            p_pow[i] = (p_pow[i-1] * P) % M;
-
-        prefix_hash[0] = 0;
-        for (int i = 1; i <= n; i++) 
-            prefix_hash[i] = (prefix_hash[i-1] +  p_pow[i]*(s[i-1] - 'a' + 1)) % M; 
-    }  
-    
 public:
-    int minimumTimeToInitialState(string word, int k) {
-        int n = word.length();
-        FillHash(word);
-        int ans = (n+k-1)/k;
-        for (int j = n-1; j > 0; j --) {
-            if (j % k != 0) continue;
-            ll hash_front = (prefix_hash[n-j] * p_pow[j]) % M;
-            ll hash_back  = (prefix_hash[n] - prefix_hash[j] + M) % M;
-            if (hash_front == hash_back) ans = min (ans, j/k);
+    vector<int> z_function(string s) {
+    int n = s.size();
+    vector<int> z(n);
+    int l = 0, r = 0;
+    for(int i = 1; i < n; i++) {
+        if(i < r) {
+            z[i] = min(r - i, z[i - l]);
         }
-        
-        return ans;
+        while(i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+            z[i]++;
+        }
+        if(i + z[i] > r) {
+            l = i;
+            r = i + z[i];
+        }
+    }
+    return z;
+}
+    int minimumTimeToInitialState(string word, int k) {
+        int n=word.length();
+        vector<int> z=z_function(word);
+        for(int i=k;i<n;i+=k){
+            if(z[i]==n-i){
+                return i/k;
+            }
+        }
+        return (n+k-1)/k;
     }
 };
