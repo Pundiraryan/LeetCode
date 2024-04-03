b839e4c8df542f43c7fda2b2afdd76e17015d689
Aryan Pundir <95073218+Pundiraryan@users.noreply.github.com>
2024-04-03 13:01:11 +0530

    Time: 247 ms (37.50%), Space: 184.5 MB (45.00%) - LeetHub

--- 1955-count-number-of-special-subsequences/1955-count-number-of-special-subsequences.cpp
+++ 1955-count-number-of-special-subsequences/1955-count-number-of-special-subsequences.cpp
@@ -1,18 +1,26 @@
 #define mod 1000000007
 class Solution {
 public:
-    int countSpecialSubsequences(vector<int>& nums) {
-        long long zs=0,zos=0,zots=0;
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                zs=(zs+zs+1)%mod;
-            }else if(nums[i]==1){
-                zos=(zos+zos+zs)%mod;
-            }else{
-                zots=(zots+zots+zos)%mod;
-            }
+    int dp[100005][5];
+    long long f(int i,int prev,vector<int>&nums){
+        if(i==nums.size())return prev==2;
+        if(dp[i][prev+1]!=-1)return dp[i][prev+1];
+        long long ans=0;
+        if(nums[i]==0){
+             ans=(ans+f(i+1,prev,nums))%mod;
+            if(prev==-1 || prev==0)ans=(ans+f(i+1,0,nums))%mod;
         }
-        return zots%mod;
+        else if(nums[i]==1){
+            ans=(ans+f(i+1,prev,nums))%mod;
+            if(prev==0 || prev==1)ans=(ans+f(i+1,1,nums))%mod;
+        }else{
+            ans=(ans+f(i+1,prev,nums))%mod;
+            if(prev==1||prev==2)ans=(ans+f(i+1,2,nums))%mod;
+        }
+        return dp[i][prev+1]= ans;
+    }
+    int countSpecialSubsequences(vector<int>& nums) {
+        memset(dp,-1,sizeof(dp));
+        return f(0,-1,nums);
     }
 };
