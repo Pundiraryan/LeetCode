6f17ce3645499a8b9d374451b3aa5f1be6314649
Aryan Pundir <95073218+Pundiraryan@users.noreply.github.com>
2024-12-24 11:13:36 +0530

    Time: 314 ms (52.96%), Space: 311.4 MB (50.47%) - LeetHub

--- 3203-find-minimum-diameter-after-merging-two-trees/3203-find-minimum-diameter-after-merging-two-trees.cpp
+++ 3203-find-minimum-diameter-after-merging-two-trees/3203-find-minimum-diameter-after-merging-two-trees.cpp
@@ -1,52 +1,38 @@
-class Solution {
-public:
-    int bfs(int src,vector<int>&ans,vector<vector<int>>&graph){
-        int val,n=graph.size();
-        queue<int>q;
-        vector<int>d(n+1,-1);
-        d[src]=0;
-        ans[src]=max(ans[src],d[src]);
-        q.push(src);
-        while(!q.empty()){
-            val=q.front();
-            q.pop();
-            for(auto x:graph[val]){
-                if(d[x]==-1){
-                    q.push(x);
-                    d[x]=1+d[val];
-                    ans[x]=max(ans[x],1+d[x]);
-                }
+    class Solution {
+    public:
+        void dfs(int node,int par,int cd,vector<int>&dis,vector<vector<int>>&graph){
+            dis[node]=cd;
+            for(auto &x:graph[node]){
+                if(x==par)continue;
+                dfs(x,node,1+cd,dis,graph);
             }
         }
-        return val;
-}
-    int minimumDiameterAfterMerge(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
-        int n=edges1.size()+1,m=edges2.size()+1;
-        // if(n==1 && m==1)return 1;
-        vector<vector<int>>graph1(n),graph2(m);
-        for(auto &x:edges1){
-            graph1[x[0]].push_back(x[1]);
-            graph1[x[1]].push_back(x[0]);
+        int diam(vector<vector<int>>&graph){
+            int n=graph.size();
+            vector<int>dis1(n);
+            dfs(0,-1,0,dis1,graph);
+            int nd=-1,mxd=-1;
+            for(int i=0;i<n;i++){
+                if(dis1[i]>mxd){
+                    nd=i;
+                    mxd=dis1[i];
+                }
+            }
+            vector<int>dis2(n);
+            dfs(nd,-1,0,dis2,graph);
+            return *max_element(dis2.begin(),dis2.end());
         }
-        for(auto &x:edges2){
-            graph2[x[0]].push_back(x[1]);
-            graph2[x[1]].push_back(x[0]);
+        int minimumDiameterAfterMerge(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
+            int n=edges1.size()+1,m=edges2.size()+1;
+            vector<vector<int>>graph1(n),graph2(m);
+            for(auto &x:edges1){
+                graph1[x[0]].push_back(x[1]);
+                graph1[x[1]].push_back(x[0]);
+            }
+            for(auto &x:edges2){
+                graph2[x[0]].push_back(x[1]);
+                graph2[x[1]].push_back(x[0]); 
+            }
+            return max({diam(graph1),diam(graph2),1+(diam(graph1)+1)/2+(diam(graph2)+1)/2});
         }
-        vector<int>ans(n,-1);
-        int fn=bfs(0,ans,graph1);
-        int sfn=bfs(fn,ans,graph1);
-        bfs(sfn,ans,graph1);
-        vector<int>md1(n);
-        for(int i=0;i<n;i++)if(ans[i])md1[i]=ans[i]-1;
-        vector<int>ans1(m,-1);
-        int fn1=bfs(0,ans1,graph2);
-        int sfn1=bfs(fn1,ans1,graph2);
-        bfs(sfn1,ans1,graph2);
-        vector<int>md2(m);
-        for(int i=0;i<m;i++)if(ans1[i])md2[i]=ans1[i]-1;
-        sort(md1.begin(),md1.end());
-        sort(md2.begin(),md2.end());
-        // cout<<md1[0]<<" || "<<md2[0]<<endl;
-        return max({md1.back(),md2.back(), md1.front()+md2.front()+1});
-    }
-};
+    };
